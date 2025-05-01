bee2d068c0b6d9c83bf95172b6100327d48d5e48
Harsh <harshatkare8@gmail.com>
2025-05-01 16:34:07 +0530

    Custom api response and error handling

---
+++ src/utils/asyncHeadler.js
@@ -0,0 +1,11 @@
+export {asyncHeadler}
+const asyncHeadler = (fun)=>async(req,res,next)=>{
+    try {
+        await fun(req,res,next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
