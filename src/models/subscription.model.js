06178de2d89c63d27ed6f8089af221b563961d87
Harsh <harshatkare8@gmail.com>
2025-05-08 00:34:16 +0530

    added watchHistory,getUserChannalProfile, etc

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -14,4 +14,4 @@ const subscriptionSchema=new Schema({
     timestamps:true
 })
 
-export const Subscription=mongoose.model("Aubscription",subscriptionSchema)
+export const Subscription=mongoose.model("Subscription",subscriptionSchema)
